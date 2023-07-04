88ab282bdd29db08b06ef84a4cbf4f733e6e51d0
Maha Abdelnabi <mahaomar@gmail.com>
2023-07-03 23:53:23 -0400

    add eventlistener to button

---
+++ script.js
@@ -0,0 +1,9 @@
+const btnEl = document.getElementById("btn")
+
+
+//add event listener to button
+function calculateAge(){
+    console.log("clicked")
+}
+
+btnEl.addEventListener("click", calculateAge)
