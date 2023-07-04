520f7dffb950f3cced234743cb4099ba505ba5f7
Maha Abdelnabi <mahaomar@gmail.com>
2023-07-04 00:34:31 -0400

    add innertext to the age output

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btnEl = document.getElementById("btn");
 const birthdayEl = document.getElementById("birthday");
+const resultEl = document.getElementById("result");
 
 
 //add event listener to button
@@ -12,7 +13,7 @@ function calculateAge(){
         alert("Please enter your birthday")
     }else{
         const age = getAge(birthdayValue);
-        console.log(age)
+        resultEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`
     }
 }
 
